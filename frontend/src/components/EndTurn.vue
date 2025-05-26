e7510b9f6e12a01c138635277d35630347984d69
Alan <lethosor@users.noreply.github.com>
2025-05-26 00:28:31 -0400

    End-turn UX improvements (#4)
    * end turn: add icons when conditions are not satisfied, refactor into component
    
    * warn if any dice have not been used
    
    * indicate how many cards have been selected to discard

--- frontend/src/components/EndTurn.vue
+++ frontend/src/components/EndTurn.vue
@@ -1,10 +1,11 @@
 <template>
   <h2>End Your Work Phase</h2>
+  <p v-if="hasUnusedDice" style="font-weight: bold">
+    <span style="color: gold">⚠</span> Some dice have not been used!
+  </p>
   <p>
-    <span v-if="gamestate.hand.size <= 10" class="check">&check;</span> Discard down to 10 cards in hand ({{
-      gamestate.hand.size
-    }}
-    / 10)
+    <BooleanIcon :value="gamestate.hand.size <= 10" />
+    Discard down to 10 cards in hand ({{ gamestate.hand.size }} / 10)
   </p>
   <div v-if="gamestate.hand.size > 10" class="card-select-area">
     <p>Select Cards To Discard</p>
@@ -19,13 +20,15 @@
         @click="selectCard(cardID)"
       />
     </div>
+    <p style="margin-top: 10px">
+      <BooleanIcon :value="result.cards.length >= gamestate.hand.size - 10" />
+      Total Selected: {{ result.cards.length }} / {{ gamestate.hand.size - 10 }} needed
+    </p>
   </div>
 
   <p>
-    <span v-if="totalResources <= 12" class="check">&check;</span> Discard down to 12 total resources ({{
-      totalResources
-    }}
-    / 12)
+    <BooleanIcon :value="totalResources <= 12" />
+    Discard down to 12 total resources ({{ totalResources }} / 12)
   </p>
   <div v-if="totalResources > 12" class="resource-select-area">
     <p>Select Resources To Discard</p>
@@ -42,13 +45,17 @@
       </select>
       <p>🔩</p>
     </div>
-    <p style="margin-top: 10px">Total Selected: {{ resourcesSelected }} / {{ totalResources - 12 }} needed</p>
+    <p style="margin-top: 10px">
+      <BooleanIcon :value="resourcesSelected >= totalResources - 12" />
+      Total Selected: {{ resourcesSelected }} / {{ totalResources - 12 }} needed
+    </p>
   </div>
 </template>
 
 <script>
 // Game state
 import { gamestate } from '../GameState.js'
+import BooleanIcon from './BooleanIcon.vue'
 import Card from './Card.vue'
 
 // Exports
@@ -60,9 +67,9 @@ export default {
     },
   },
   components: {
+    BooleanIcon,
     Card,
   },
-  mounted() {},
   data() {
     return {
       gamestate,
@@ -84,6 +91,12 @@ export default {
      */
     resourcesSelected() {
       return this.result.energy + this.result.metal
+    },
+    hasUnusedDice() {
+      return (
+        gamestate.state.players[gamestate.playerID].dice.length > 0 ||
+        gamestate.state.players[gamestate.playerID].numDice > 0
+      )
     },
   },
   methods: {
